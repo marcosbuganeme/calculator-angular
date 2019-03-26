3b29ffa5e96e1b5a923b40400250319c37eae7c6
Marcos Buganeme <marcos.after@gmail.com>
2019-03-25 22:12:34 -0300

    add tests

--- src/app/calculadora/components/calculadora/calculadora.component.spec.ts
+++ src/app/calculadora/components/calculadora/calculadora.component.spec.ts
@@ -2,6 +2,7 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { CalculadoraComponent } from './calculadora.component';
 import { CalculadoraService } from '../../services/calculadora.service';
+import { By } from '@angular/platform-browser';
 
 describe('CalculadoraComponent', () => {
   let component: CalculadoraComponent;
@@ -25,7 +26,29 @@ describe('CalculadoraComponent', () => {
     fixture.detectChanges();
   });
 
-  it('should create', () => {
+  it('deve criar o componente', () => {
     expect(component).toBeTruthy();
   });
+
+  it('deve somar dois elementos 3 + 2 = 5', () => {
+    let botaoTres = fixture.debugElement.query(By.css('#btn3'))
+    let botaoSoma = fixture.debugElement.query(By.css('#btnSoma'))
+    let botaoDois = fixture.debugElement.query(By.css('#btn2'))
+    let botaoCalcular = fixture.debugElement.query(By.css('#btnCalcular'))
+    let display = fixture.debugElement.query(By.css('#display'))
+
+    botaoTres.triggerEventHandler('click', null)
+    fixture.detectChanges()
+
+    botaoSoma.triggerEventHandler('click', null)
+    fixture.detectChanges()
+
+    botaoDois.triggerEventHandler('click', null)
+    fixture.detectChanges()
+
+    botaoCalcular.triggerEventHandler('click', null)
+    fixture.detectChanges()
+
+    expect(display.nativeElement.value).toEqual('5')
+  })
 });
